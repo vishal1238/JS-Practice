76169b247eec23af18a1c11b4262a1a8d17e4dc5
vishal1238 <webervishal@gmail.com>
2026-02-16 13:30:16 +0530

    file38.js

---
+++ practice/PartB/file38.js
@@ -0,0 +1,29 @@
+// let d = new Date()
+// console.log(d);
+
+//get methods
+// console.log(d.getDate());
+// console.log(d.getMonth());
+// console.log(d.getFullYear());
+// console.log(d.getHours());
+// console.log(d.getDay());
+// console.log(d.getMinutes());
+// console.log(d.getSeconds());
+// console.log(d.getMilliseconds());
+
+
+// let d = new Date(20026,1,1,10,10,10)
+// console.log(d);
+
+
+let d = new Date()
+d.getFullYear(2025)
+// d.setMonth(11)
+// d.setDate(21)
+// d.getTime(10)
+// d.setHours(10)
+// d.getMinutes(10)
+// d.setSeconds(10)
+// d.setMilliseconds(1)
+// d.setTime(9999900000)
+console.log(d);
