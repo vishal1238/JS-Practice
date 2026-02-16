d261c71b9c0299d3e037d788da96e90c898244f9
vishal1238 <webervishal@gmail.com>
2026-02-16 11:34:34 +0530

    file37

---
+++ practice/PartB/file37.js
@@ -0,0 +1,12 @@
+let str = "Hello world"
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str.indexOf('l'));
+console.log(str.lastIndexOf('l'));
+console.log(str.includes("W"));
+console.log(str.startsWith("H"));
+console.log(str.endsWith("d"));
+console.log(str.slice(1,9));
+console.log(str.substring(1,9));
+
