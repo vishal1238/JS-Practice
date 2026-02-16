a47169a612fbb57ec91a94f9a592080e78d9d99e
vishal1238 <webervishal@gmail.com>
2026-02-16 10:49:01 +0530

    file updated

--- practice/PartB/file33.js
+++ practice/PartB/file33.js
@@ -1,22 +1,32 @@
+// const student = {
+//     name:"John",
+//     age:21
+// }
+// const {name,age} = student
+// console.log(name)
+// console.log(age)
+
+// const {name:userName} = student
+// console.log(userName)
+
+
+// const student = {
+//     name:"John",
+//     age:21,
+//     city:"Jalandhar"
+// }
+// const {city="Amritsar"} = student
+// console.log(city)
+
 const student = {
-    name: "John",
-    age: 21,
-    city: "Jalandhar",
+    name:"John",
+    age:21,
     marks:{
-        math: 60,
-        scienec: 90
+        math:60,
+        science:90
     }
 }
-
-// const {name,age} = student
-// console.log(name);
-// console.log(age);
-
-// const {name: userName} = student
-// console.log(userName);
-
-const {city = "Amritsar"} = student
-console.log(city);
-
-const {marks:{marks}} = student
-console.log(math);
+const {marks} = student
+console.log(marks)
+const {marks:{math}} = student
+console.log(math)
