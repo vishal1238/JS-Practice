8a6a0570e3cf3ca3b7a3c616dd299bd4c8ef12f4
vishal1238 <webervishal@gmail.com>
2026-02-16 13:53:46 +0530

    file39

---
+++ practice/PartC/file39.js
@@ -0,0 +1,19 @@
+//Hosting with var
+var name1 = "John"
+console.log(name1);
+
+var name = "John"
+var name
+console.log(name);
+name = "john"
+
+
+//Hosting with let and const
+console.log(name);
+let name = "John"
+
+greet()
+function greet() {
+    console.log("Hello World");
+}
+greet()
