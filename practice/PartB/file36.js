22f3bcb154277afcf4c020b191923e63a04fa476
vishal1238 <webervishal@gmail.com>
2026-02-16 11:27:48 +0530

    file36

---
+++ practice/PartB/file36.js
@@ -0,0 +1,16 @@
+let num = 10.1
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+
+// console.log(num.toFixed(2));
+
+num = "1232"
+console.log(isNaN(num));
+
+console.log(Math.random());
+
+console.log(Math.max(5,3,2,8,4,9));
+console.log(Math.min(5,3,2,8,4,9));
+
+console.log(Math.pow(10,2));
